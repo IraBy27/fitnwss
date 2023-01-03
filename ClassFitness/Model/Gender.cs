8383b7c9fe93699d30846e7a9e21d655e80b4f72
i.bulenkova@bk.ru <i.bulenkova@bk.ru>
2023-01-03 09:21:17 +0300

    Добавьте файлы проекта.

---
+++ ClassFitness/Model/Gender.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassFitness.Model
+{
+    public class Gender
+    {
+        public string Name { get; }
+        public Gender (string name)
+        {
+          if (string.IsNullOrWhiteSpace(name)) 
+            { throw new ArgumentNullException("name is not null",nameof(name)); }
+          Name= name;
+        }
+        public override string ToString()
+        {
+            return Name;
+
+        }
+
+    }
+}
---
+++ ClassFitness/Model/User.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassFitness.Model
+{
+    public class User
+    {
+        public string Name { get; }
+        public Gender Gender { get; }
+        public DateTime DateBirth { get; }
+        public  double Height { get; set; }
+        public double Width { get; set; }
+        public User(string name, Gender gender, DateTime dateBirth, double height, double width)
+        {
+            #region Проверка региона
+
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException("name is not null",nameof(name));
+            }
+            if (gender == null) 
+            {
+                throw new ArgumentNullException("gender is not null",nameof(gender));
+            }
+            if (dateBirth <DateTime.Parse("01.01.1922")|| dateBirth >= DateTime.Now) {
+                throw new ArgumentException("невозможная дата рождения", nameof(dateBirth));
+            }
+            if (height <= 50) {
+                throw new ArgumentException("рост не может быть меньше равен 0", nameof(height));
+            }
+        
+            if(width <= 0) {
+                throw new ArgumentException("вес не может быть меньше равен 0", nameof(width));
+            }
+            #endregion
+            Name = name;
+            Gender = gender;
+            DateBirth = dateBirth;
+            Height = height;
+            Width = width;
+        }
+    }
+}
